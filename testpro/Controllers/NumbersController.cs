486c6c7b68827ec5f238e910b0dc5352cffc4e7e
Theophilus Unekwu <theophilusu@examroom.ai>
2024-07-20 20:59:22 +0100

    added Numbers controller

---
+++ testpro/Controllers/NumbersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace testpro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumbersController : ControllerBase
+    {
+        // GET: api/<NumbersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
