ca581171ed34a4348ddd7ec5a2ced66cfb0ad023
Theophilus Unekwu <theophilusu@examroom.ai>
2024-07-20 17:39:08 +0100

    configured swagger settings for prod

--- testpro/Program.cs
+++ testpro/Program.cs
@@ -17,7 +17,7 @@ namespace testpro
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
